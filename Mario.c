15f8d8fc974520e337be8374cd4e8e12056675b2
MatanBarhoum <88832406+MatanBarhoum@users.noreply.github.com>
2022-06-21 22:52:40 +0300

    Create Mario.c

---
+++ Mario.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height, column, row;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for (row = 0; row < height; row++)
+    {
+        for (int space = 0; space < height - row - 1; space++)
+        {
+            printf(" ");
+        }
+        // printing the hashes
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (column = 0; column <= row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+}
